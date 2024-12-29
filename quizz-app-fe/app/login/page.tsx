754e2bbb69bf53eb4545723158e57cd62803c81e
AS221412 <alizadeh1412@gmail.com>
2024-12-29 20:08:06 +0100

    login page implemented {without login form}

--- quizz-app-fe/app/login/page.tsx
+++ quizz-app-fe/app/login/page.tsx
@@ -8,17 +8,21 @@ const Page = () => {
             {/* Linkes Div - Blauer Bereich */}
             <div className="bg-seaBlue w-1/2 flex items-center justify-center">
                 <div className="text-white font-bold text-2xl text-center">
-                    <Image  src="/logo.svg" alt="Logo" width={200} height={200}/>
+                    <Image className="ml-5" src="/logo.svg" alt="Logo" width={200} height={200}/>
                     <p>Spielend leicht</p>
                     <p>dein Wissen</p>
                     <p>Abfragen</p>
                 </div>
             </div>
 
-            {/* Rechtes Div */}
-            <div className="w-1/2  flex mt-[200px] justify-center">
-                <h1 className="text-5xl text-seaBlue font-bold">Willkommen</h1>
+            {/* Rechtes Div - Weißer Bereich */}
+            <div className="w-1/2 flex flex-col items-center  justify-center">
+                <h1 className="text-5xl text-seaBlue font-bold mb-4">Willkommen</h1>
+                <h2 className="text-2xl text-seaBlue">Login</h2>
             </div>
+
+
+
         </div>
     )
 }
