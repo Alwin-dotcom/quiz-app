522bb8e601fe2cb1bf7dac923eef3e2db141ac85
AS221412 <alizadeh1412@gmail.com>
2025-01-07 20:17:54 +0100

    Sidebar implemented

--- quizz-app-fe/app/layout.tsx
+++ quizz-app-fe/app/layout.tsx
@@ -1,7 +1,7 @@
 // RootLayout.tsx
 'use client'
 import "./globals.css";
-import { Roboto } from "next/font/google";
+import {Roboto} from "next/font/google";
 import {usePathname} from "next/navigation";
 import Sidebar from "./Components/Sidebar";
 
@@ -10,7 +10,6 @@ const roboto = Roboto({
     weight: ["400", "500", "700", "900"],
     subsets: ["latin"],
 });
-
 
 export default function RootLayout({children,}: Readonly<{ children: React.ReactNode; }>) {
 
@@ -23,7 +22,7 @@ export default function RootLayout({children,}: Readonly<{ children: React.React
         <body
             className={` ${roboto.className} antialiased`}>
         <div className="flex h-screen">
-            {showSidebar && <Sidebar />}
+            {showSidebar && <Sidebar/>}
             <div className="flex-1">{children}</div>
         </div>
         </body>
