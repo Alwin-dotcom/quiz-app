f2aa960786d5b3ccf79fba8d98a25096070c562f
AS221412 <alizadeh1412@gmail.com>
2025-01-07 21:23:18 +0100

    Leaderboard Komponente hinzugefügt

--- quizz-app-fe/app/page.tsx
+++ quizz-app-fe/app/page.tsx
@@ -1,9 +1,18 @@
 import React from 'react'
+import LeaderboardIcon from '@mui/icons-material/Leaderboard';
 
 const page = () => {
     return (
         <div>
-            <h1>HomePage</h1>
+            {/*Leaderboard div*/}
+            <div className="flex items-center justify-center w-[800px] p-4 text-seaBlue
+                mx-auto bg-[#D9D9D9] text-2xl font-semibold mt-[200px] border rounded-xl shadow">
+                <span>Leaderboard</span>
+                <LeaderboardIcon sx={{
+                    fontSize: 30,
+                    ml: 2,
+                }}/>
+            </div>
         </div>
     )
 }
