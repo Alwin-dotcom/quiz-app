9b3d3878f7d15bb4692bce2f83bd118e7d8a6c3f
Alwin-dotcom <alwin.fuerst@web.de>
2025-01-10 12:20:16 +0100

    add flyway scripts for table creation

---
+++ quizz-app-be/src/main/resources/db/migration/V2__CREATE_TABLES.sql
@@ -0,0 +1,11 @@
+create table question_answer (
+id bigint not null,
+answers jsonb,
+createdAt timestamp,
+creator text,
+module text,
+question text,
+primary key (id));
+
+
+create sequence question_answer_SEQ start with 1 increment by 50;
