768621eea5bb30306ae031e709898a33bf438b63
AS221412 <alizadeh1412@gmail.com>
2025-01-16 20:49:28 +0100

    adding configurations for cors

--- quizz-app-fe/next.config.ts
+++ quizz-app-fe/next.config.ts
@@ -1,40 +1,23 @@
-import type {NextConfig} from "next";
-
+import type {NextConfig} from 'next'
 
 const nextConfig: NextConfig = {
-    async rewrites() {
-        return [
-            {
-                source: "/api/:path*",
-                destination: "http://localhost:8080/:path*",
-            },
-        ];
-    },
     async headers() {
         return [
             {
+                // matching all API routes
                 source: "/api/:path*",
                 headers: [
-                    {
-                        key: "Access-Control-Allow-Origin",
-                        value: "*",
-                    },
-                    {
-                        key: "Access-Control-Allow-Methods",
-                        value: "GET, POST, PUT, DELETE, OPTIONS",
-                    },
+                    {key: "Access-Control-Allow-Credentials", value: "true"},
+                    {key: "Access-Control-Allow-Origin", value: "*"},
+                    {key: "Access-Control-Allow-Methods", value: "GET,DELETE,PATCH,POST,PUT"},
                     {
                         key: "Access-Control-Allow-Headers",
-                        value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version",
+                        value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version"
                     },
-                    {
-                        key: "Access-Control-Allow-Credentials",
-                        value: "true",
-                    },
-                ],
-            },
-        ];
-    },
-};
+                ]
+            }
+        ]
+    }
+}
 
 module.exports = nextConfig;
