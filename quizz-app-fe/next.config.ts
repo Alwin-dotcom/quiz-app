4639aa8c57fa4dfccc99a3b8f93b8a048a2cb95c
AS221412 <alizadeh1412@gmail.com>
2025-01-16 20:08:01 +0100

    adding configurations for cors

--- quizz-app-fe/next.config.ts
+++ quizz-app-fe/next.config.ts
@@ -1,7 +1,40 @@
-import type { NextConfig } from "next";
+import type {NextConfig} from "next";
+
 
 const nextConfig: NextConfig = {
-  /* config options here */
+    async rewrites() {
+        return [
+            {
+                source: "/api/:path*",
+                destination: "http://localhost:8080/:path*",
+            },
+        ];
+    },
+    async headers() {
+        return [
+            {
+                source: "/api/:path*",
+                headers: [
+                    {
+                        key: "Access-Control-Allow-Origin",
+                        value: "*",
+                    },
+                    {
+                        key: "Access-Control-Allow-Methods",
+                        value: "GET, POST, PUT, DELETE, OPTIONS",
+                    },
+                    {
+                        key: "Access-Control-Allow-Headers",
+                        value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version",
+                    },
+                    {
+                        key: "Access-Control-Allow-Credentials",
+                        value: "true",
+                    },
+                ],
+            },
+        ];
+    },
 };
 
-export default nextConfig;
+module.exports = nextConfig;
