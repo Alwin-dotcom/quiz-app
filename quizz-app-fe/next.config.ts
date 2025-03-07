25e102219ed0b3f29ea61325a9645fb695bc0c3d
Ali Alizadeh <122991302+AliCodesX@users.noreply.github.com>
2025-03-07 15:45:50 +0100

    Update next.config.ts

--- quizz-app-fe/next.config.ts
+++ quizz-app-fe/next.config.ts
@@ -6,7 +6,7 @@ const nextConfig: NextConfig = {
         ignoreDuringBuilds: true,
     },
     env: {
-        NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL || 'http://localhost:8080',
+        NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL,
     },
     async headers() {
         return [
